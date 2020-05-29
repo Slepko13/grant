7b308d8757557b86635df0cf7f73ca7a881fcefc
Mykola Slepko <slepko13@gmail.com>
2020-05-29 16:39:00 +0300

    Updates

---
+++ precache-manifest.797892a91f00ad2eb8affc043a420239.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b161c7e2b2e81552951879ea01757954",
+    "url": "/grant/index.html"
+  },
+  {
+    "revision": "86f0bf97bf8ad042007d",
+    "url": "/grant/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "0b18bf774d2e7fb82495",
+    "url": "/grant/static/css/main.7728e2a5.chunk.css"
+  },
+  {
+    "revision": "86f0bf97bf8ad042007d",
+    "url": "/grant/static/js/2.2e0f18b7.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/grant/static/js/2.2e0f18b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b18bf774d2e7fb82495",
+    "url": "/grant/static/js/main.17f7f48b.chunk.js"
+  },
+  {
+    "revision": "780fa118cd85c41b1717",
+    "url": "/grant/static/js/runtime-main.631a4d71.js"
+  },
+  {
+    "revision": "83a51f5131deebbca77001d241c06d78",
+    "url": "/grant/static/media/COAX.83a51f51.png"
+  },
+  {
+    "revision": "431a69e42f6771f1677d8be03ef93758",
+    "url": "/grant/static/media/CodeElves.431a69e4.png"
+  },
+  {
+    "revision": "2ba3791451380b57623f12f7db627da7",
+    "url": "/grant/static/media/Eleks.2ba37914.png"
+  },
+  {
+    "revision": "d9e11e6d7b23c80e35bbbff8bde47631",
+    "url": "/grant/static/media/ITRally.d9e11e6d.png"
+  },
+  {
+    "revision": "62723ac45f1227e1f944ae2fe16c130f",
+    "url": "/grant/static/media/Netfully.62723ac4.jpg"
+  },
+  {
+    "revision": "869c135e070160a7623aa3648a47fc0b",
+    "url": "/grant/static/media/Ukietech.869c135e.png"
+  },
+  {
+    "revision": "ba5c07155144999aea61eba25781231c",
+    "url": "/grant/static/media/bvblogik.ba5c0715.png"
+  },
+  {
+    "revision": "db36d87e200e2c6031a9bde88455309c",
+    "url": "/grant/static/media/devlight.db36d87e.png"
+  },
+  {
+    "revision": "6dddddc0b4726becc525fc7794cfcf6b",
+    "url": "/grant/static/media/futura-pt-bold.6dddddc0.ttf"
+  },
+  {
+    "revision": "c7f1106be884b5027c6d93deec8ca00a",
+    "url": "/grant/static/media/futura-pt-light.c7f1106b.ttf"
+  },
+  {
+    "revision": "540c81bc4ca37ae1efe4b60de51e753f",
+    "url": "/grant/static/media/futura-pt-medium.540c81bc.ttf"
+  },
+  {
+    "revision": "199cab50999adf1d26488c92cafde320",
+    "url": "/grant/static/media/illustration1.199cab50.svg"
+  },
+  {
+    "revision": "bfc45ba53c6b459f616a39235879b1b2",
+    "url": "/grant/static/media/illustration2.bfc45ba5.svg"
+  },
+  {
+    "revision": "41ccddbd019c96b21d06be08c26b0515",
+    "url": "/grant/static/media/illustration3.41ccddbd.svg"
+  },
+  {
+    "revision": "a3cec3f501efca822474aadd7e6974ea",
+    "url": "/grant/static/media/insilico.a3cec3f5.png"
+  },
+  {
+    "revision": "b5d68f72df102ac1fa7b1a757c35ddda",
+    "url": "/grant/static/media/it-cluster.b5d68f72.png"
+  },
+  {
+    "revision": "b1ea38c977ec52dabbb6bcfcf8934b30",
+    "url": "/grant/static/media/leadcapitalgroup.b1ea38c9.png"
+  },
+  {
+    "revision": "7a8a8f54ba0a3823aab9a9c2a66e121d",
+    "url": "/grant/static/media/softjourn.7a8a8f54.png"
+  },
+  {
+    "revision": "a7576a502fb8fb9dad59c8082003f788",
+    "url": "/grant/static/media/softserve.a7576a50.png"
+  },
+  {
+    "revision": "f8d2071af3f90a446df4620e318fe23d",
+    "url": "/grant/static/media/tenantcloud.f8d2071a.png"
+  },
+  {
+    "revision": "89ed98433c600c2e7124fc502f79a27a",
+    "url": "/grant/static/media/wtf.89ed9843.png"
+  }
+]);
